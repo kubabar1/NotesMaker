9b28bf9598cd03d35991dc6152e89ca497adb986
Jakub Barański <kubabar1@interia.pl>
2019-01-18 22:54:08 +0100

    Added project structure

---
+++ notes-maker-api/src/main/java/com/od/notesmaker/NotesmakerApplication.java
@@ -0,0 +1,14 @@
+package com.od.notesmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesmakerApplication.class, args);
+	}
+
+}
+
